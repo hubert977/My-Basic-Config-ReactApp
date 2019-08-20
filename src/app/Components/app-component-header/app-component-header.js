f55a0f181689e6468d8e8e26dcc5cbefbf450a37
hubert.wozniak977@gmail.com <hubert.wozniak977@gmail.com>
2019-08-20 16:27:58 +0200

    React-Basic-Config

---
+++ src/app/Components/app-component-header/app-component-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app-component-header.scss';
+class Header extends React.Component {
+    render()
+    {
+        return(
+        <header>
+        </header>
+        )
+    }
+}
+export default Header;
